1f7695bed4ccee6bc21c10c427aa1aa31602973c
Naoto Sakakibara <naotosakaki@gmail.com>
2012-09-18 10:20:01 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Garden::Application.config.secret_token = 'd60e081feb644224f2281c998b46e8ed8d8a2d2201c35b7a4dc73141cd6c25207e4aa5e1b009e68fbd8374d68c0d123fb388df989cda70342749c14c5589c28d'
